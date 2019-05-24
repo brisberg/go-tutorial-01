3e3b89461b39cd3c5e489e24c2920dbd64814f6b
Brandon Risberg <brandon.risberg@gmail.com>
2019-05-24 02:13:49 -0700

    Print Method on the 'deck' type using a receiver

--- cards/deck.go
+++ cards/deck.go
@@ -1,5 +1,14 @@
 package main
 
+import "fmt"
+
 // Create a new type of 'deck'
 // which is a slice of strings
 type deck []string
+
+// (d deck) specifies the 'receiver' for the function
+func (d deck) print() {
+	for _, card := range d {
+		fmt.Println(1, card)
+	}
+}
