8417d3037fa4ccf3a1b15a32032da422279b3bb5
Brandon Risberg <brandon.risberg@gmail.com>
2019-05-24 13:00:08 -0700

    ContactInfo substruct

--- structs/main.go
+++ structs/main.go
@@ -5,13 +5,23 @@ import "fmt"
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
+}
+
+type contactInfo struct {
+	email   string
+	zipCode int
 }
 
 func main() {
-	var alex person
-
-	alex.firstName = "Alex"
-	alex.lastName = "Anderson"
+	alex := person{
+		firstName: "Alex",
+		lastName:  "Anderson",
+		contact: contactInfo{
+			email:   "alex@gmail.com",
+			zipCode: 54321,
+		},
+	}
 
 	fmt.Println(alex)
 	fmt.Printf("%+v", alex)
