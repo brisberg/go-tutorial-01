e73d0659c16aefe0fc33406288bc60b504f5aff6
Brandon Risberg <brandon.risberg@gmail.com>
2019-05-24 02:10:08 -0700

    Custom deck type in multiple files

--- cards/main.go
+++ cards/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	cards := []string{newCard(), newCard(), "Ace of Diamonds"}
+	cards := deck{newCard(), newCard(), "Ace of Diamonds"}
 	cards = append(cards, "Six of Spades")
 
 	for i, card := range cards {
