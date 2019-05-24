278c9da23f8d089daef50e10732f6100ff931ee2
Brandon Risberg <brandon.risberg@gmail.com>
2019-05-24 13:33:39 -0700

    Added update method using a pointer receiver to update the struct

--- structs/main.go
+++ structs/main.go
@@ -23,6 +23,14 @@ func main() {
 		},
 	}
 
-	fmt.Println(alex)
-	fmt.Printf("%+v", alex)
+	alex.updateName("Alexander")
+	alex.print()
+}
+
+func (p *person) updateName(newFirstName string) {
+	p.firstName = newFirstName
+}
+
+func (p *person) print() {
+	fmt.Printf("%+v", p)
 }
