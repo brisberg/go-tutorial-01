1cf256ee32cf63c64f493e1df53acc5192e6e4de
Brandon Risberg <brisberg@google.com>
2019-05-28 12:17:25 -0700

    for loop syntax and function param for maps

--- maps/main.go
+++ maps/main.go
@@ -3,16 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	colors := make(map[string]string)
-
-	// colors := map[string]string{
-	// 	"red":   "#ff0000",
-	// 	"green": "#00ff00",
-	// }
-
-	colors["white"] = "#ffffff"
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#00ff00",
+		"white": "#ffffff",
+	}
 
 	fmt.Println(colors)
+	printMap(colors)
+}
 
-	delete(colors, "white") // delete this key value
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println(color, hex)
+	}
 }
