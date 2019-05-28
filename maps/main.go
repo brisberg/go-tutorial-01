89fb9848f3bf1b9a4ff394511c42d04521c241bd
Brandon Risberg <brisberg@google.com>
2019-05-28 12:11:58 -0700

    using 'make' to initialize the map

--- maps/main.go
+++ maps/main.go
@@ -3,10 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	colors := map[string]string{
-		"red":   "#ff0000",
-		"green": "#00ff00",
-	}
+	colors := make(map[string]string)
+
+	// colors := map[string]string{
+	// 	"red":   "#ff0000",
+	// 	"green": "#00ff00",
+	// }
+
+	colors["white"] = "#ffffff"
 
 	fmt.Println(colors)
 }
