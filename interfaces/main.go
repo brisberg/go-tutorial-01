24a0b9bc823ba0fae32a5f2f22b6225c77b6f0f5
Brandon Risberg <brisberg@google.com>
2019-05-28 12:42:32 -0700

    common interface 'bot' between the two structs

--- interfaces/main.go
+++ interfaces/main.go
@@ -1,10 +1,20 @@
 package main
 
+import "fmt"
+
+type bot interface {
+	getGreeting() string
+}
+
 type englishBot struct{}
 type spanishBot struct{}
 
 func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
 
+	printGreeting(eb)
+	printGreeting(sb)
 }
 
 func (englishBot) getGreeting() string {
@@ -13,4 +23,8 @@ func (englishBot) getGreeting() string {
 
 func (spanishBot) getGreeting() string {
 	return "Hola!" // custom code for spanishbot
+}
+
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
 }
