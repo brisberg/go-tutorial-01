5f20f08489db7a1b8e9bb36b651c6f691d0d7fac
Brandon Risberg <brisberg@google.com>
2019-05-28 17:36:34 -0700

    Implemented assignment2

--- assignment2/main.go
+++ assignment2/main.go
@@ -1,20 +1,42 @@
 package main
 
-import (
-)
+import "fmt"
 
 type triangle struct {
-	length int,
+	base   int
+	height int
 }
 
 type square struct {
-	length int,
+	sideLength int
 }
 
 type shape interface {
-	getArea() float64,
+	getArea() float64
 }
 
 func main() {
+	t := triangle{
+		base:   10,
+		height: 5,
+	}
 
+	s := square{
+		sideLength: 6,
+	}
+
+	fmt.Println(printArea(t))
+	fmt.Println(printArea(s))
+}
+
+func (t triangle) getArea() float64 {
+	return 0.5 * float64(t.base*t.height)
+}
+
+func (s square) getArea() float64 {
+	return float64(s.sideLength * s.sideLength)
+}
+
+func printArea(s shape) float64 {
+	return s.getArea()
 }
