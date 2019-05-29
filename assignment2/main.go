f577bf07f21c73bea3c3c44f29aea05a40f68313
Brandon Risberg <brisberg@google.com>
2019-05-28 17:30:08 -0700

    Initial commit on assignment2

---
+++ assignment2/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+)
+
+type triangle struct {
+	length int,
+}
+
+type square struct {
+	length int,
+}
+
+type shape interface {
+	getArea() float64,
+}
+
+func main() {
+
+}
